abd6a9d60a134ca6c7874674e0189b0c2a994047
Lukas Hamm <ideallygrey@tuta.io>
2021-12-01 20:53:27 -0500

    Update main.py

---
+++ Puzzle1/main.py
@@ -0,0 +1,11 @@
+#outline:
+#takes input, puts it into list
+#var checking set to 2
+#while var checking <= total list
+    #var current set to var checking
+    #var past set to var checking - 1
+    #if var current > var past
+        #then
+            #var total = var total + 1
+    #var checking + 1
+#print var total
